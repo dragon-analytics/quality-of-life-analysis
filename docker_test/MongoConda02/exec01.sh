aaf8a514a607756add88b32657d1b4ae4c71fd0f
adfmb <actuario.alfredo.mb@gmail.com>
2018-05-31 21:39:14 -0500

    remove volumens from docker-compose and update exec01.sh

--- docker_test/MongoConda02/exec01.sh
+++ docker_test/MongoConda02/exec01.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
- mkdir /temp
- git clone https://github.com/dragon-analytics/quality-of-life-analysis.git temp;
- mv -r temp/* mediapy/notebooks/
- rm -rf temp
+mkdir temp
+git clone https://github.com/dragon-analytics/quality-of-life-analysis.git temp/
+cp -r temp/* mediapy/notebooks/.
+
+
+ #mkdir /temp
+ #git clone https://github.com/dragon-analytics/quality-of-life-analysis.git temp;
+ #mv -r temp/* mediapy/notebooks/
+ #rm -rf temp
